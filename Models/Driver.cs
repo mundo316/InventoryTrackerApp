b737b22c329a850a876ee02543f72823a42b47a4
mundo316 <raymundo.garcia316@yahoo.com>
2020-02-23 11:58:08 -0600

    added driver model

---
+++ Models/Driver.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone.Models
+{
+public class Driver 
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int id {get; set; }
+        public string truckNo { get; set; }
+        public string driverName { get; set; }
+    }
+}
